a22ddb441473e10860b0c5c016c8c4df61e0b3f0
TFL <conish@o2.pl>
2013-08-25 12:05:19 +0200

    cleaning up

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>DEV<?=$title;?></title>
+        <title><?=$title;?></title>
         <link href="<?=base_url();?>css/style.css" rel="stylesheet" type="text/css" media="screen" />
     </head>
     <body>
